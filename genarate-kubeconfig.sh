1c54c994833ea888fdc73e8c841e2aee58269d68
sabarish196 <sabarishreddy7@gmail.com>
2021-10-26 15:07:38 -0700

    Update genarate-kubeconfig.sh

--- genarate-kubeconfig.sh
+++ genarate-kubeconfig.sh
@@ -21,56 +21,55 @@ create_target_folder() {
 
 create_service_account() {
     echo -e "\\nCreating a service account in ${NAMESPACE} namespace: ${SERVICE_ACCOUNT_NAME}"
-    kubectl create sa "${SERVICE_ACCOUNT_NAME}" --namespace "${NAMESPACE}"
+    oc create sa "${SERVICE_ACCOUNT_NAME}" --namespace "${NAMESPACE}"
 }
 
 get_secret_name_from_service_account() {
     echo -e "\\nGetting secret of service account ${SERVICE_ACCOUNT_NAME} on ${NAMESPACE}"
-    SECRET_NAME=$(kubectl get sa "${SERVICE_ACCOUNT_NAME}" --namespace="${NAMESPACE}" -o json | jq -r .secrets[].name | grep -v docker)
+    SECRET_NAME=$(oc get sa "${SERVICE_ACCOUNT_NAME}" --namespace="${NAMESPACE}" -o yaml | grep -A2 secrets: | grep -v dockercfg | grep name | awk '{print $3}')
     echo "Secret name: ${SECRET_NAME}"
 }
 
 extract_ca_crt_from_secret() {
     echo -e -n "\\nExtracting ca.crt from secret..."
-    kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o json | jq \
-    -r '.data["ca.crt"]' | base64 -D > "${TARGET_FOLDER}/ca.crt"
+    oc get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o yaml | grep -v service-ca.crt | grep ca.crt | awk '{print $2}' | base64 -D > "${TARGET_FOLDER}/ca.crt"
     printf "done"
 }
 
 get_user_token_from_secret() {
     echo -e -n "\\nGetting user token from secret..."
-    USER_TOKEN=$(kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o json | jq -r '.data["token"]' | base64 -D)
+    USER_TOKEN=$(oc get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o yaml | grep token: | awk '{print $2}' | base64 -D)
     printf "done"
 }
 
 set_kube_config_values() {
-    context=$(kubectl config current-context)
+    context=$(oc config current-context)
     echo -e "\\nSetting current context to: $context"
 
-    CLUSTER_NAME=$(kubectl config get-contexts "$context" | awk '{print $3}' | tail -n 1)
+    CLUSTER_NAME=$(oc config get-contexts "$context" | awk '{print $3}' | tail -n 1)
     echo "Cluster name: ${CLUSTER_NAME}"
 
-    ENDPOINT=$(kubectl config view \
+    ENDPOINT=$(oc config view \
     -o jsonpath="{.clusters[?(@.name == \"${CLUSTER_NAME}\")].cluster.server}")
     echo "Endpoint: ${ENDPOINT}"
 
     # Set up the config
     echo -e "\\nPreparing k8s-${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-conf"
     echo -n "Setting a cluster entry in kubeconfig..."
-    kubectl config set-cluster "${CLUSTER_NAME}" \
+    oc config set-cluster "${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}" \
     --server="${ENDPOINT}" \
     --certificate-authority="${TARGET_FOLDER}/ca.crt" \
     --embed-certs=true
 
     echo -n "Setting token credentials entry in kubeconfig..."
-    kubectl config set-credentials \
+    oc config set-credentials \
     "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}" \
     --token="${USER_TOKEN}"
 
     echo -n "Setting a context entry in kubeconfig..."
-    kubectl config set-context \
+    oc config set-context \
     "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}" \
     --cluster="${CLUSTER_NAME}" \
@@ -78,7 +77,7 @@ set_kube_config_values() {
     --namespace="${NAMESPACE}"
 
     echo -n "Setting the current-context in the kubeconfig file..."
-    kubectl config use-context "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
+    oc config use-context "${SERVICE_ACCOUNT_NAME}-${NAMESPACE}-${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}"
 }
 
@@ -90,7 +89,7 @@ get_user_token_from_secret
 set_kube_config_values
 
 echo -e "\\nAll done! Test with:"
-echo "KUBECONFIG=${KUBECFG_FILE_NAME} kubectl get pods"
+echo "KUBECONFIG=${KUBECFG_FILE_NAME} oc get pods"
 echo "you should not have any permissions by default - you have just created the authentication part"
 echo "You will need to create RBAC permissions"
-KUBECONFIG=${KUBECFG_FILE_NAME} kubectl get pods
+KUBECONFIG=${KUBECFG_FILE_NAME} oc get pods
